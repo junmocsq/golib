cd7e1e9c124ad781a69ff092fd199d02a07f7f04
junmo <junmocsq@163.com>
2020-03-02 14:23:09 +0800

    添加工具库

---
+++ library/tools.go
@@ -0,0 +1,78 @@
+package library
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"sort"
+	"strconv"
+	"time"
+)
+
+// map参数转换为url参数形式
+func MapToUrlParams(baseUrl string, m map[string]string) (string, error) {
+	params := url.Values{}
+	Url, err := url.Parse(baseUrl)
+	if err != nil {
+		return "", err
+	}
+	for k, v := range m {
+		params.Set(k, v)
+	}
+	//如果参数中有中文参数,这个方法会进行URLEncode
+	Url.RawQuery = params.Encode()
+	urlPath := Url.String()
+	return urlPath, nil
+}
+
+/**
+获取提交参数数组
+*/
+func CpOpenToken(params map[string]string, token string) error {
+	params["timestamp"] = strconv.Itoa(int(time.Now().Unix()))
+	sortKeyArr := make([]string, 0)
+	for k, _ := range params {
+		sortKeyArr = append(sortKeyArr, k)
+	}
+	sort.Strings(sortKeyArr)
+	var s string
+	for _, v := range sortKeyArr {
+		s += v + params[v]
+	}
+	s += token
+	t, err := Md5(s)
+	if err != nil {
+		return err
+	}
+	params["token"] = t
+	return nil
+}
+
+func Md5(ss string) (res string, err error) {
+
+	md5Ctx := md5.New()
+	_, err = md5Ctx.Write([]byte(ss))
+	if err != nil {
+		return
+	}
+	res = hex.EncodeToString(md5Ctx.Sum(nil))
+	return
+}
+
+func HttpGet(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		// handle error
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		return nil, err
+	}
+	return body, nil
+}
